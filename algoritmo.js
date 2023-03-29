5025a815285cafb9c6b99ce59f7b7fffd256db6d
Sofia Medina <medinasofiaa1@gmail.com>
2023-03-28 23:36:50 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+document.querySelector("body").innerHTML = `
+
+<h1>Practica 1.1</h1>
+
+    <h2> Titulo h2</h2>
+
+    <h3>Titulo h3</h3>
+
+    <img src="https://th.bing.com/th/id/R.3a54cb76129a456647bb000211e7aa66?rik=YYu9FxIhlzJvVA&riu=http%3a%2f%2fimagenesbonitas.bosquedefantasias.com%2fwp-content%2fuploads%2f2015%2f08%2fim%c3%a1genes-de-gatos3.jpg&ehk=tgEXNQJ0Fb8yfi2Zw6njlzjBIQgocoFMcCAALbYiGnI%3d&risl=&pid=ImgRaw&r=0"
+        alt=" gatito ">
+
+        <img src="imagenes/kitten.jpg" alt="Gatito 2">
+
+
+`;
